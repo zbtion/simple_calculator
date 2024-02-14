71420fa3835835b878a8a916039f6f05e27e0dd9
zbtion <fhyhfj13@gmail.com>
2024-02-14 21:05:40 +0800

    dot, change sign implementation

--- Controller.py
+++ Controller.py
@@ -2,42 +2,52 @@
 class Controller:
     def __init__(self, screen):
         self.screen = screen
-        self.label = ""
-        self.current = ""
+        self.exp = ""
+        self.cur = ""
     
     def recv_message(self, message):
-        if message == "C" or self.label == "Error":
-            self.label = ""
-            self.current = ""
+        if message == "C" or self.cur == "Error":
+            self.exp = ""
+            self.cur = ""
 
         elif message == "del":
-            if self.current != "":
-                self.current = self.current[:-1]
+            if self.cur != "":
+                self.cur = self.cur[:-1]
             else:
-                self.label = self.label[:-1]
+                self.exp = self.exp[:-1]
 
         elif message == "=":
             try:
-                self.label = str(eval(self.label+self.current))
+                self.cur = str(eval(self.exp+self.cur))
             except:
-                self.label = "Error"
-            self.current = ""
+                self.cur = "Error"
+            self.exp = ""
+                
         
         elif message == "+/-":
-            pass    # not implemented yet
+            if not self.is_operator(self.cur):
+                self.cur = str(-1 * float(self.cur))
+            else:
+                self.exp += self.cur
+                self.cur = "-"
     
         elif message == ".":
-            pass   # not implemented yet
+            if not self.is_operator(self.cur):
+                if "." not in self.cur:
+                    self.cur += "."
+            else:
+                self.exp += self.cur
+                self.cur = "0."
 
         elif self.is_operator(message):
-            if not self.is_operator(self.current):
-                self.label += self.current                
-            self.current = message
+            if not self.is_operator(self.cur):
+                self.exp += self.cur                
+            self.cur = message
         
         else:
-            self.current += message
+            self.cur += message
 
-        self.screen.set_label(self.label + self.current)
+        self.screen.set_label(self.exp + self.cur)
 
     def is_operator(self, message):
         return message in "+-*/%"
